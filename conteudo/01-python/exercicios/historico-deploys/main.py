ed47a0aaa47872df6af88aad11752292c14907f5
Renan Martins <renanmartins1999@hotmail.com>
2024-11-23 21:47:16 -0300

    feat: add resolução historico de deploys

---
+++ conteudo/01-python/exercicios/historico-deploys/main.py
@@ -0,0 +1,12 @@
+data_dos_deploys = ["2024-11-10",
+    "2024-11-07",
+    "2024-10-30",
+    "2024-10-03",
+    "2024-10-01", 
+    "2024-09-25"]
+
+data_dos_deploys.insert(0, "2024-11-20")
+data_dos_deploys.pop()
+
+for i in data_dos_deploys:
+    print(i)
